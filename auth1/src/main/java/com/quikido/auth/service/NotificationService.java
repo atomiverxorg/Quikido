9e05f9e42053b3aceaa2aa013c3bd73265248353
atomiverxorg <code-atomiverx@atomiverx.com>
2024-12-16 09:47:47 +0530

    Step 17: Push Notifications for Real-Time Updates

--- auth1/src/main/java/com/quikido/auth/service/NotificationService.java
+++ auth1/src/main/java/com/quikido/auth/service/NotificationService.java
@@ -1,5 +1,9 @@
 package com.quikido.auth.service;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +12,19 @@ public class NotificationService {
     }
 
     public void notifyPassenger(String passengerEmail, String s) {
+    }
+
+    public void sendNotification(String token, String title, String body) {
+        Message message = Message.builder()
+                .setToken(token)
+                .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+                .build();
+
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+            System.out.println("Notification sent: " + response);
+        } catch (FirebaseMessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
